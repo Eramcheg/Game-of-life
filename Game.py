d43d57163a07c0d09bee0518ef561cc16cbf14fd
eramcheg <eramcheg@gmail.com>
2022-12-07 14:09:30 +0100

    Created: Simple translate of 1 square on the grid

--- Game.py
+++ Game.py
@@ -15,8 +15,11 @@ class Game:
         # for i in range(20):
         #     for j in range(20):
 
+    def fucking_move(self,surface,i):
+        pygame.draw.rect(surface, "white", (30 * i, 0, 30, 30))
 
-    def drawGrid(self,w,rows,surface):
+
+    def drawGrid(self,w,rows,surface,i):
         size=w//rows
         x=0
         y=0
@@ -25,27 +28,36 @@ class Game:
             y=y+size
             pygame.draw.line(surface,(255,255,255),(x,0),(x,w))
             pygame.draw.line(surface,(255,255,255),(0,y),(w,y))
-        for i in range(20):
-            pygame.draw.rect(surface,"white",(30*i,0,30,30))
+        #self.fucking_move(surface,i)
+        #pygame.draw.rect(surface, "white", (30, 0, 30, 30))
 
-    def redrawWindow(self, surface):
+    def draw(self,surface,i):
+        pygame.draw.rect(surface, (255, 255, 255), (60, 60*i, 60, 60))
+    def redrawWindow(self, surface,i):
         surface.fill((0,0,0))
-        self.drawGrid(width,rows,surface)
+        self.drawGrid(width,rows,surface,i)
+        self.draw(surface,i)
         pygame.display.update()
 
     def startGame(self):
         global width,rows
         width=600
-        rows=20
+        rows=10
         height=600
         win=pygame.display.set_mode((width,width))
         clock=pygame.time.Clock()
 
         flag=True
+        i=0
         while flag:
-            pygame.time.delay(50)
-            clock.tick(10)
-            self.redrawWindow(win)
+            pygame.time.delay(500)
+            clock.tick(60)
+
+            self.redrawWindow(win,i)
+
+
+            #self.fucking_move(win,i)
+            i+=1
 
 game=Game()
 game.startGame()
