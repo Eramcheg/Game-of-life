81224b15cc651cd7b7d178c1a2dd0568887f9313
eramcheg <eramcheg@gmail.com>
2022-12-08 19:26:58 +0100

    Fixed: Settings Fixed circular import

--- Menu.py
+++ Menu.py
@@ -1,11 +1,14 @@
 import tkinter as tk
-import PauseMenu
+import GameClass
 import threading
 from tkinter import *
+import Settings
+
+
 class Menu:
-     root = tk.Tk()
+
      def startMenu(self):
-        root=self.root
+        root = tk.Tk()
         root.geometry("600x600")
         root.title("Menu")
         root.configure(background="black")
@@ -16,10 +19,10 @@ class Menu:
         buttonFrame.columnconfigure(2, weight=1)
 
         buttonFrame.pack(pady=200)
-        startGameBtn=tk.Button(buttonFrame,text="Start Game",font=('Arial',18),command=self.startGame)
+        startGameBtn=tk.Button(buttonFrame,text="Start Game",font=('Arial',18),command= lambda: self.startGame(root))
         startGameBtn.grid(row=0,column=1, sticky=tk.E+tk.W)
 
-        settingsBtn = Button(buttonFrame, text="Settings", font=('Arial', 18))
+        settingsBtn = Button(buttonFrame, text="Settings", font=('Arial', 18),command=lambda : self.SettingsMethod(root))
         settingsBtn.grid(row=2,column=1,sticky=tk.E+tk.W)
 
         quitBtn=tk.Button(buttonFrame,text="Quit",font=('Arial', 18),command= self.quitMenu)
@@ -27,19 +30,29 @@ class Menu:
 
         root.mainloop()
 
-     def startGame(self):
-        self.root.destroy()
+     def startGame(self,root):
+        root.destroy()
         t1=threading.Thread(target=self.openGame)
         t1.start()
         t1.join()
 
      def openGame(self):
-        game = PauseMenu.Game()
+        game = GameClass.Game()
         game.startGame()
 
+     def SettingsMethod(self,root):
+         root.destroy()
+         t2=threading.Thread(target=self.openSettings)
+         t2.start()
+         t2.join()
+     def openSettings(self):
+
+         settin=Settings.SettingsC()
+         settin.windowSettings()
      def quitMenu(self):
         exit()
-     #   exit()
+
+
 st=Menu()
 st.startMenu()
 
---
+++ Settings.py
@@ -0,0 +1,44 @@
+import sys
+import tkinter as tk
+import threading
+
+
+class SettingsC:
+
+    def windowSettings(self):
+        root = tk.Tk()
+        root.geometry("600x600")
+        root.title("Settings")
+        root.configure(background="black")
+
+        frame = tk.Frame(root)
+        frame.columnconfigure(0, weight=1)
+        frame.columnconfigure(1, weight=1)
+        frame.columnconfigure(2, weight=1)
+
+        frame.pack(pady=200)
+
+        label1=tk.Label(frame,text="Setting 1")
+        textField=tk.Text(frame, font=('Arial',14), height=1,width=10)
+        label1.grid(row=0,column=0, sticky=tk.E+tk.W)
+        textField.grid(row=0, column=1, sticky=tk.E + tk.W)
+
+        backBtn=tk.Button(frame,text="Back",font=('Arial',18),command=lambda:self.backMenu(root))
+        backBtn.grid(row=2,column=0,sticky=tk.E + tk.W)
+
+        quitBtn = tk.Button(frame, text="Quit", font=('Arial', 18), command=self.quitSettings)
+        quitBtn.grid(row=2, column=1, sticky=tk.E + tk.W)
+        root.mainloop()
+
+    def backMenu(self,root):
+        root.destroy()
+        t1 = threading.Thread(target=self.openMenu)
+        t1.start()
+        t1.join()
+
+    def openMenu(self):
+        from Menu import Menu
+        menu = Menu()
+        menu.startMenu()
+    def quitSettings(self):
+       exit()
