b69794b4e7b0d7f20b28c2668f07f87069eaa019
eramcheg <eramcheg@gmail.com>
2022-12-09 16:31:23 +0100

    Developed: Finished Settings

--- Settings.py
+++ Settings.py
@@ -10,9 +10,10 @@ class SettingsC:
         self.resolutionValues.append(800)
         self.resolutionValues.append(1000)
 
-        self.rowsValues=list()
+        self.rowsValues = list()
         self.rowsValues.append(10)
         self.rowsValues.append(20)
+
     def windowSettings(self):
         file = open('settings.json')
         data = json.load(file)
@@ -30,62 +31,65 @@ class SettingsC:
 
         frame.pack(pady=200)
 
-        label1 = tk.Label(frame, text="Life speed")
-        textField = tk.Scale(frame, from_=80, to=1000, orient='horizontal')
-        textField.set(data['speed'])
-        label1.grid(row=0, column=0, sticky=tk.E + tk.W)
-        textField.grid(row=0, column=1, sticky=tk.E + tk.W)
+        SpeedLabel = tk.Label(frame, text="Life speed")
+        SpeedTextBox = tk.Scale(frame, from_=80, to=1000, orient='horizontal')
+        SpeedTextBox.set(data['speed'])
+        SpeedLabel.grid(row=0, column=0, sticky=tk.E + tk.W)
+        SpeedTextBox.grid(row=0, column=1, sticky=tk.E + tk.W)
 
+        ResolutionLabel = tk.Label(frame, text="Window width")
+        ResolutionCombobox = ttk.Combobox(frame, values=self.resolutionValues)
+        ResolutionCombobox.set(data['resolution'])
 
-
-        label2 = tk.Label(frame, text="Window width")
-        combobox2 = ttk.Combobox(frame, values=self.resolutionValues)
-        combobox2.set(data['resolution'])
-
-
-        if combobox2.get() == '600':
+        if ResolutionCombobox.get() == '600':
             self.rowsValues.append(30)
-        elif combobox2.get() == '800':
+        elif ResolutionCombobox.get() == '800':
             self.rowsValues.append(40)
-        elif combobox2.get() == '1000':
+        elif ResolutionCombobox.get() == '1000':
             self.rowsValues.append(50)
-        label3 = tk.Label(frame, text="Rows amount")
-        combobox = ttk.Combobox(frame, values=self.rowsValues)
+        RowsLabel = tk.Label(frame, text="Rows amount")
+        RowsCombobox = ttk.Combobox(frame, values=self.rowsValues)
 
-        combobox.set(data['rows'])
-        label3.grid(row=2, column=0, sticky=tk.E + tk.W)
-        combobox.grid(row=2, column=1, sticky=tk.E + tk.W)
+        RowsCombobox.set(data['rows'])
+        RowsLabel.grid(row=2, column=0, sticky=tk.E + tk.W)
+        RowsCombobox.grid(row=2, column=1, sticky=tk.E + tk.W)
 
-        combobox2.bind('<<ComboboxSelected>>', lambda event, x=combobox: self.valueChanged(event,x))
-        label2.grid(row=1, column=0, sticky=tk.E + tk.W)
-        combobox2.grid(row=1, column=1, sticky=tk.E + tk.W)
+        ResolutionCombobox.bind('<<ComboboxSelected>>', lambda event, x=RowsCombobox: self.valueChanged(event, x))
+        ResolutionLabel.grid(row=1, column=0, sticky=tk.E + tk.W)
+        ResolutionCombobox.grid(row=1, column=1, sticky=tk.E + tk.W)
 
-        backBtn = tk.Button(frame, text="Back", font=('Arial', 18),
-                            command=lambda: self.backMenu(root, int(textField.get()), int(combobox.get()),int(combobox2.get())))
+        backBtn = tk.Button(frame, text="Back & Save", font=('Arial', 18),
+                            command=lambda: self.backMenu(root, int(SpeedTextBox.get()), int(RowsCombobox.get()),
+                                                          int(ResolutionCombobox.get())))
         backBtn.grid(row=3, column=0, sticky=tk.E + tk.W)
 
         quitBtn = tk.Button(frame, text="Quit", font=('Arial', 18), command=self.quitSettings)
         quitBtn.grid(row=3, column=1, sticky=tk.E + tk.W)
         root.mainloop()
 
-    def valueChanged(self,event,combobox):
+    def valueChanged(self, event, combobox):
         self.rowsValues.pop()
-        if event.widget.get()=='600':
+        if event.widget.get() == '600':
             self.rowsValues.append(30)
-        elif event.widget.get()=='800':
+        elif event.widget.get() == '800':
             self.rowsValues.append(40)
+        elif event.widget.get() == '1000':
+            self.rowsValues.append(50)
         combobox.configure(values=self.rowsValues)
         combobox.set(self.rowsValues[0])
         combobox.update()
 
-    def backMenu(self, root, speed, rows,resolution):
+    def backMenu(self, root, speed, rows, resolution):
         root.destroy()
         file = open('settings.json')
         data = json.load(file)
         data["speed"] = speed
-        data["rows"] = rows
+        if rows not in self.rowsValues:
+            data["rows"] = self.rowsValues[0]
+        else:
+            data["rows"] = rows
         if resolution not in self.resolutionValues:
-            data["resolution"]=self.resolutionValues[0]
+            data["resolution"] = self.resolutionValues[0]
         else:
             data["resolution"] = resolution
         with open("settings.json", "w") as outfile:
